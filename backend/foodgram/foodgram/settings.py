cff0749e5098b79797c09b0c7171ff3006ff3a41
Матвей Слуцких <poustplay@yandex.ru>
2023-02-15 21:34:36 +0300

    media fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/rest-framework/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/rest-framework')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

8bb4c7518aaea6564cfc42df2ebff6d1e5e4fe30
Матвей Слуцких <poustplay@yandex.ru>
2023-02-16 12:50:50 +0300

    createrecipefix

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,19 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
-                    TagViewSet)
+from .views import (CustomUserViewSet, IngredientsViewSet, RecipeViewSet,
+                    TagsViewSet)
 
 app_name = 'api'
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet)
-router.register('tags', TagViewSet)
-router.register('ingredients', IngredientViewSet)
+router.register('tags', TagsViewSet)
+router.register('ingredients', IngredientsViewSet)
 router.register('users', CustomUserViewSet)
 
 router.register(r'recipes/(?P<recipe_id>\d+)/', RecipeViewSet)
 router.register(r'users/(?P<user_id>\d+)/', CustomUserViewSet)
-router.register(r'tags/(?P<tag_id>\d+)/', TagViewSet)
+router.register(r'tags/(?P<tag_id>\d+)/', TagsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

453b5db706ede8925e5f56bacda3761775d668a5
Матвей Слуцких <poustplay@yandex.ru>
2023-02-16 17:05:14 +0300

    dataloader

--- backend/foodgram/static/data/dataloader.py
+++ backend/foodgram/static/data/dataloader.py
@@ -5,7 +5,7 @@ from django.db import transaction
 from recipes.models import Ingredient
 
 json_file_path = (
-    'C:/app/static/data/ingredients.json')
+    'ingredients.json')
 
 with open(json_file_path, encoding='utf-8') as f:
     data = json.load(f)
